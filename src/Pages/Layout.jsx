3c31dd65ebb1f23174f7d315eef4971146d08d0d
Ksenia Dedik <kseniya.dedik@gmail.com>
2023-06-28 18:20:38 +0200

    update photo

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Navigation from '../NavBar';
+import Navigation from './NavBar';
 
 function Layout() {
   return (
--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import { Link } from '@mui/material';
 
 const pages = [
@@ -25,21 +21,10 @@ const pages = [
     link: '/contact'
   }
 ];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBar position="static" /*sx={{ backgroundColor: 'red' }}*/>
+    <AppBar position="static">
       <Container maxWidth="xl" sx={{ backgroundColor: '#DE3163' }}>
         <Toolbar disableGutters>
           <Typography
@@ -76,35 +61,7 @@ function ResponsiveAppBar() {
               </Link>
             ))}
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="K" src="./dog.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right'
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right'
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}>
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          <Avatar alt="Ks" src="./ks_photo_cropped.jpg" sx={{ transform: 'scale(1.5)' }} />
         </Toolbar>
       </Container>
     </AppBar>
