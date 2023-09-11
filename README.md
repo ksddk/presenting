cd6dca4021eb1b25f47e9c8c9bc4a14cc7b86479
Ksenia Dedik <kseniya.dedik@gmail.com>
2023-09-11 14:27:57 +0200

    done all todos and smth with hamb menu

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 # TODO
 
-## Add links:
-
-- contact info done
-- github link done
-
-## CV
-
-Create a summary of your work (resume in pdf): available on the web application as a separate page and downloadable by click on a button "Download CV"
-
-## Contact form
-
-Create a form that allows users to send a message to you from the web application ?????
-
 ## TBD
 
 - skills representation (animations)
--- src/Pages/Navigation.tsx
+++ src/Pages/Navigation.tsx
@@ -1,5 +1,7 @@
+import { slide as Menu } from 'react-burger-menu';
 import Title from './Assets/Title.svg';
 import styles from './Navigation.module.css';
+import { useState } from 'react';
 
 interface IPages {
   name: string;
@@ -17,8 +19,59 @@ const pages: IPages[] = [
   }
 ];
 
+// var menuStyles = {
+//   bmBurgerButton: {
+//     position: 'fixed', //кнопка - полосочки меню
+//     width: '26px',
+//     height: '20px',
+//     left: '26px',
+//     top: '26px'
+//   },
+//   bmBurgerBars: {
+//     background: '#333333' //цвет полосочек
+//   },
+//   bmBurgerBarsHover: {
+//     // background: '#a90000',
+//     background: 'blue',
+//     border: '3px solid red' //пока хз
+//   },
+//   bmCrossButton: {
+//     height: '24px', //пока хз
+//     width: '24px'
+//   },
+//   bmCross: {
+//     background: '#D9D9D9' //цвет крестика
+//   },
+//   bmMenuWrap: {
+//     position: 'fixed',
+//     height: '100%'
+//   },
+//   bmMenu: {
+//     background: '#CCACE2',
+//     padding: '2.5em 1.5em 0',
+//     fontSize: '1.15em'
+//   },
+//   bmMorphShape: {
+//     fill: '#373a47'
+//   },
+//   bmItemList: {
+//     padding: '0.8em'
+//   },
+//   bmItem: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     textDecoration: 'none',
+//     color: 'black'
+//   },
+//   bmOverlay: {
+//     background: 'rgba(0, 0, 0, 0.1)'
+//   }
+// };
+
 function Navigation() {
   // TODO: add hamburger here on 768px
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className={styles.navigation}>
       <a href="/" className={styles.svgContainer}>
@@ -32,6 +85,16 @@ function Navigation() {
           </a>
         ))}
       </div>
+
+      {/* <Menu styles={menuStyles} isOpen={isMenuOpen}>
+        <div>
+          {pages.map(({ name, link }) => (
+            <a key={name} href={link} className="menu-item">
+              {name}
+            </a>
+          ))}
+        </div>
+      </Menu> */}
     </div>
   );
 }
